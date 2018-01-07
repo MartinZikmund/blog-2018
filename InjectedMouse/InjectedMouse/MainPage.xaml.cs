9f5bbc15840b0f11db5d01e13aaba271c48ea7cf
Martin Zikmund <martinzikmund@sphereline.com>
2018-01-07 11:09:38 +0100

    Mouse input injection sample

---
+++ InjectedMouse/InjectedMouse/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Preview.Injection;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InjectedMouse
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            InjectedInputMouseInfo info = new InjectedInputMouseInfo();
+            info.MouseOptions = InjectedInputMouseOptions.Wheel;
+            unchecked
+            {
+                info.MouseData = (uint)-500;
+            }
+
+            InputInjector inputInjector = InputInjector.TryCreate();
+            inputInjector.InjectMouseInput(new[] { info });
+        }
+    }
+}
