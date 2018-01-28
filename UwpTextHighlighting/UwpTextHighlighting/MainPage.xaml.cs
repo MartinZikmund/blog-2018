dcb6a070b53fa96efb67cb495be8aafe402614c6
Martin Zikmund <martinzikmund@live.com>
2018-01-28 22:24:53 +0100

    UWP text highlighting

---
+++ UwpTextHighlighting/UwpTextHighlighting/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpTextHighlighting
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            HighlightText();
+        }
+
+        public void HighlightText()
+        {
+            TextRange textRange = new TextRange() { StartIndex = 3, Length = 10 };    
+            
+            TextHighlighter highlighter = new TextHighlighter()
+            {
+                Background = new SolidColorBrush(Colors.Yellow),
+                Ranges = { textRange }
+            };
+
+            RichBlock.TextHighlighters.Add(highlighter);
+        }
+    }
+}
