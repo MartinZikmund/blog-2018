0e341164c421a8814b15ea4c8579ff92b445312c
Martin Zikmund <martinzikmund@sphereline.com>
2018-03-12 18:38:02 +0100

    Week selection

---
+++ WeekSelectionCalendarView/WeekSelectionCalendarView/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Xaml.Interactivity;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeekSelectionCalendarView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+private void CalendarView_CalendarViewDayItemChanging(CalendarView sender, CalendarViewDayItemChangingEventArgs args)
+{
+    var behavior = new WeekHighlightBehavior();
+    behavior.CalendarControl = CalendarView;
+    Interaction.GetBehaviors(args.Item).Clear();
+    Interaction.GetBehaviors(args.Item).Add(behavior);
+}
+    }
+}
---
+++ WeekSelectionCalendarView/WeekSelectionCalendarView/WeekHighlightBehavior.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace WeekSelectionCalendarView
+{
+public class WeekHighlightBehavior : Behavior
+{
+    public CalendarView CalendarControl
+    {
+        get { return (CalendarView)GetValue(CalendarProperty); }
+        set { SetValue(CalendarProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Calendar.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty CalendarProperty =
+        DependencyProperty.Register("CalendarControl", typeof(CalendarView), typeof(WeekHighlightBehavior), new PropertyMetadata(0));
+        
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        CalendarControl.SelectedDatesChanged += Calendar_SelectedDatesChanged;
+    }
+
+    private void Calendar_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+    {
+        var dayItem = this.AssociatedObject as CalendarViewDayItem;
+        var calendar = CultureInfo.CurrentUICulture.Calendar;
+        bool highlight = false;
+        foreach (var date in args.AddedDates)
+        {
+            if (calendar.GetWeekOfYear(date.DateTime, CalendarWeekRule.FirstDay, DayOfWeek.Monday) ==
+                calendar.GetWeekOfYear(dayItem.Date.DateTime, CalendarWeekRule.FirstDay, DayOfWeek.Monday))
+            {
+                highlight = true;
+            }
+        }
+
+        if (highlight)
+        {
+            dayItem.Background = new SolidColorBrush(Colors.Yellow);
+        }
+        else
+        {
+            dayItem.Background = null;
+        }
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        CalendarControl.SelectedDatesChanged -= Calendar_SelectedDatesChanged;
+    }
+}
+}
