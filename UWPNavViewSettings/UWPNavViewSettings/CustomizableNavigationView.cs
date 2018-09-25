81ae6f96beb32999ab83fee46e7824ba5d09ad53
Martin Zikmund <martinzikmund@sphereline.com>
2018-09-25 13:47:52 +0200

    Improved code to use Dependency property

--- UWPNavViewSettings/UWPNavViewSettings/CustomizableNavigationView.cs
+++ UWPNavViewSettings/UWPNavViewSettings/CustomizableNavigationView.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace UWPNavViewSettings
 {
-public class CustomizableNavigationView : NavigationView
-{
-    protected override void OnApplyTemplate()
+    public class CustomizableNavigationView : NavigationView
     {
-        base.OnApplyTemplate();
-        var settingsItem = (NavigationViewItem)GetTemplateChild("SettingsNavPaneItem");
-        settingsItem.Content = "Custom text";
+        private const string SettingsNavigationViewItemName = "SettingsNavPaneItem";
+
+        private NavigationViewItem _settingsItem;
+
+        public static readonly DependencyProperty SettingsItemTextProperty = DependencyProperty.Register(
+           nameof(SettingsItemText), typeof(string), typeof(CustomizableNavigationView), new PropertyMetadata(default(string), SettingsItemTextChanged));
+
+        public string SettingsItemText
+        {
+            get => (string)GetValue(SettingsItemTextProperty);
+            set => SetValue(SettingsItemTextProperty, value);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _settingsItem = (NavigationViewItem)GetTemplateChild(SettingsNavigationViewItemName);
+            UpdateSettingsItemText();
+        }
+
+        private static void SettingsItemTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is CustomizableNavigationView navigationView)
+            {
+                navigationView.UpdateSettingsItemText();
+            }
+        }
+
+        private void UpdateSettingsItemText()
+        {
+            if (_settingsItem != null)
+            {
+                _settingsItem.Content = SettingsItemText ?? "";
+            }
+        }
     }
-}
 }
