345b2f61f92a7c56ef818ab8d89cf35120972971
Martin Zikmund <martinzikmund@sphereline.com>
2018-09-25 12:41:16 +0200

    Customizable navigation view

---
+++ UWPNavViewSettings/UWPNavViewSettings/CustomizableNavigationView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPNavViewSettings
+{
+public class CustomizableNavigationView : NavigationView
+{
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        var settingsItem = (NavigationViewItem)GetTemplateChild("SettingsNavPaneItem");
+        settingsItem.Content = "Custom text";
+    }
+}
+}
