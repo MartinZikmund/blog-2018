55d4530306a3dbf34991f86e0f1abb7795beacc4
Martin Zikmund <martinzikmund@sphereline.com>
2018-11-20 00:14:16 +0100

    Xamarin.Forms compiled bindings

---
+++ XFCompiledBindings/MyAwesomeApp/MyAwesomeApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace MyAwesomeApp.ViewModels
+{
+    public class HomeViewModel : BindableObject
+    {
+        private string _greeting = "Hello";
+
+        public string Greeting
+        {
+            get => _greeting;
+            set
+            {
+                _greeting = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
